7c9704a3302715b14d80a278c91cd43ddb34883e
KristopherMakuch <makuchkris@gmail.com>
2018-06-20 12:40:55 -0400

    Update reports.run.R

--- R/reports.run.R
+++ R/reports.run.R
@@ -23,8 +23,8 @@
 
 reports.run <- function(profileId, reportId){
 
-  req.url <- paste0("https://www.googleapis.com/dfareporting/v2.8/userprofiles/",profileId,"/reports/",reportId,"/run")
-  response <- fromJSON(api.request(req.url,querystring="synchronous=true",method='POST'))
+  req.url <- paste0("https://www.googleapis.com/dfareporting/v3.0/userprofiles/",profileId,"/reports/",reportId,"/run")
+  response <- fromJSON(api.request(req.url,querystring="synchronous=true",method='GET'))
   
   # now that the report has run, get the report
   return(files.get(response$id,response$reportId))
